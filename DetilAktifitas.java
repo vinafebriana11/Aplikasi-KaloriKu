5421568aafdb8289f38ceff8c09fd93594292e96
vinafebriana11 <101590691+vinafebriana11@users.noreply.github.com>
2022-04-30 09:17:18 +0700

    Add files via upload

---
+++ DetilAktifitas.java
@@ -0,0 +1,13 @@
+package com.tugasaplikasi.kaloriku.penghitungkalori;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class DetilAktifitas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detil_aktifitas);
+    }
+}
